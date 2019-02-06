d150214bf3405248de21c0f15da48ca17ec65cd9
Jakob Aarøe Dam <jakob.dam@systematic.com>
2019-02-06 08:53:09 +0100

    Update repos

--- init.el
+++ init.el
@@ -43,8 +43,7 @@
 
 ;; Package Initialization - Package Manager
 (require 'package)
-(setq package-archives '(("org"       . "http://orgmode.org/elpa/")
-                         ("gnu"       . "http://elpa.gnu.org/packages/")
+(setq package-archives '(("gnu"       . "http://elpa.gnu.org/packages/")
                          ("melpa"     . "http://melpa.org/packages/")))
 (package-initialize)
 
